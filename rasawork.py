ae6c8304ae39954611e322503512b9e82c55a1ad
ptarasul18 <32715140+ptarasul18@users.noreply.github.com>
2020-03-14 11:00:30 -0500

    Create rasawork.py

---
+++ rasawork.py
@@ -0,0 +1,10 @@
+#Run these
+#!pip uninstall prompt-toolkit --yes
+#!pip install --upgrade prompt-toolkit==1.0.15
+#!pip install rasa
+
+#https://rasa.com/docs/getting-started/
+#!rasa init
+
+#!cat ./sample_data/README.md
+#!rasa shell
